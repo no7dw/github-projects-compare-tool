5a3bf973956e629a4ff57397c6960901555cdab7
dengwei <dengwei@dcxs100.com>
2014-12-14 22:11:59 +0800

    compare by value input

--- public/javascripts/getcomp.js
+++ public/javascripts/getcomp.js
@@ -3,13 +3,18 @@ $(function(){
         $('#result').html('comparing');
         $.ajax({
             type: "GET",
-            url: "/gpm",    
+            url: "/gpm",
+            data:"url1="+$('#url1').val()+'&url2='+ $('#url2').val()   ,
             success: function(data){
                 console.log('data',data);
                 $('#result').empty();   
                 var html = '<table class="table table-bordered table-hover" data-toggle="table" data-cache="true" data-height="299"><tbody><tr><th></th><th>loopback</th><th>sailsjs</th></tr>' ;
-                $.each(data, function(index, comment){                                    
+                $.each(data, function(index, eachdata){                            
+                                           
+                    console.log(index, eachdata);                               
                     html += "<tr><td></td><td>"+ data[index].Star +"</td><td>"+data[index].Fork+"</td></tr>"
+                    
+                
                 });
                 html += '</tbody></table>';
                 $('#result').html(html);
--- routes/gpm.js
+++ routes/gpm.js
@@ -2,14 +2,18 @@ var express = require('express');
 var router = express.Router();
 var gpm = require('../controller/req');
 
-url1 = "https://github.com/balderdashy/sails/";
-url2 = "https://github.com/strongloop/loopback";
 
-urllist = [url1,url2];
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  var data = [{"url":"https://github.com/strongloop/loopback","Star":2710,"Fork":237},{"url":"https://github.com/balderdashy/sails/","Star":8604,"Fork":951}];
+  var url1,url2;
+// url1 = "https://github.com/balderdashy/sails/";
+// url2 = "https://github.com/strongloop/loopback";
+  console.log(req.query);
+  url1=req.query.url1;
+  url2=req.query.url2;
+  urllist = [url1,url2];
+  // var data = [{"url":"https://github.com/strongloop/loopback","Star":2710,"Fork":237},{"url":"https://github.com/balderdashy/sails/","Star":8604,"Fork":951}];  
   // return res.json(data);
   gpm(urllist, function(err, result) {
   	res.json(result);
