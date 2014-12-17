75621918a5fbb524412f7c61424443aeb5be5ab9
no7dw <no7david@gmail.com>
2014-12-17 23:30:10 +0800

    auto trans data ,without hardcode

--- public/javascripts/getcomp.js
+++ public/javascripts/getcomp.js
@@ -5,15 +5,19 @@ $(function(){
             type: "GET",
             url: "/gpm",
             data:"url1="+$('#url1').val()+'&url2='+ $('#url2').val()   ,
-            success: function(data){
-                console.log('data',data);
+            success: function(data){                
                 $('#result').empty();   
                 var html = '<table class="table table-bordered table-hover" data-toggle="table" data-cache="true" data-height="299"><tbody><tr><th></th><th>loopback</th><th>sailsjs</th></tr>' ;
                 $.each(data, function(index, eachdata){                                                                    
-                    console.log(index, eachdata);                               
-                    html += "<tr><td>" + index +  "</td> <td>"+ eachdata[0] + "</td><td>" + eachdata[1] + "</td></tr>"                                    
+                                                   
+                    html += "<tr><td>" + index +  "</td>";
+                    $.each(eachdata, function(index, content){
+                        html += "<td>" + content + "</td>"
+                    });
+
+                    html+="</tr>" 
                 });
-                html += '</tbody></table>';
+                html += '</tbody></table>';            
                 $('#result').html(html);
             }
         });
